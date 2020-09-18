1ed62564872956abd4ddb45b8fcde45e2d1cbb3c
wanderson <wandersonramos740@gmail.com>
2020-09-18 20:13:10 -0300

    colisao

--- js/script.js
+++ js/script.js
@@ -33,10 +33,13 @@ function preload() {
     { frameWidth: 60, frameHeight: 50 }
   );
 
-
+  this.load.image('coli','assets/Colisao.png')
 }
 
 function create() {
+
+  //criando grupo que grada a colisão do ataque
+  coli = this.physics.add.group();
 
   //plano de fundo
   var bac = this.add.sprite(400, 300, 'bg').setScale(2.8);
@@ -62,7 +65,10 @@ function create() {
   // aumentando o chinta
   chinta.setScale(2.0);
 
-  //criando o chinta parado
+  //verdadeiro se o personagem estiver atacando
+  chinta.attack = false;
+
+  //criando o chinta se movendo
   this.anims.create({
     key: 'move',
     frames: this.anims.generateFrameNumbers('chintaMove', { start: 17, end: 22 }),
@@ -70,42 +76,90 @@ function create() {
     repeat: -1
   });
 
+  //criando o chinta atacando
+  this.anims.create({
+    key: 'attack',
+    frames: this.anims.generateFrameNumbers('chintaMove', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: 0
+  });
 
   // pegar o input do teclado
   cursors = this.input.keyboard.createCursorKeys();
+
+  /*
+  physics.add.overlap(coli, this.stars, function(player, enemy) {
+    player.destroy();
+    enemy.destroy();
+  });*/
 
 }
 
 function update() {
 
+  //sempre setar velocidade para zero
   chinta.setVelocity(0);
+  //flage que vai indicar quando o player estiver 
+  //em movimento
   var move = false;
   //dando play na animação
   //object.anims.play('left', true);
-  
-  if (cursors.left.isDown){
-      chinta.setVelocityX(-160);
 
-      move = true;
+  //só aceita outro comanda se não estiver atacando
+  if(!chinta.attack){
+    //space é o botão de atack
+    if(cursors.space.isDown){
+      chinta.attack = true;
 
-  }else if (cursors.right.isDown){
-      chinta.setVelocityX(160);
+      chinta.anims.play('attack',true);
 
-      move = true;
-  }if (cursors.up.isDown){
-      chinta.setVelocityY(-50);
+      var pos = 10;
+      //var colis = null;
+      //ativar colisão
+      colis = this.physics.add.sprite(chinta.x+pos,chinta.y,'coli');
+      //colis.setScale(2.5);
+      //aumentar altura da colisão
+      colis.displayHeight = 80;
 
-      move = true;
-  }else if (cursors.down.isDown){
-      chinta.setVelocityY(50);
+      //adicionar colisão ao grupo
+      coli.add(colis);
 
-      move = true;
+      //quando a animação de ataque terminar 
+      //ficar parado
+      chinta.once('animationcomplete', () => {
+        //colocar animação idle aqui
+        chinta.anims.play('move', false);
+        //desabilitar colisão
+        coli.clear(true,true);
+        chinta.attack = false;
+      });
+
+    }else{
+      if (cursors.left.isDown){
+          chinta.setVelocityX(-160);
+
+          move = true;
+      }else if (cursors.right.isDown){
+          chinta.setVelocityX(160);
+
+          move = true;
+      }if (cursors.up.isDown){
+          chinta.setVelocityY(-50);
+
+          move = true;
+      }else if (cursors.down.isDown){
+          chinta.setVelocityY(50);
+
+          move = true;
+      }
+
+      //se estiver se movendo ativar animação de movimento
+      if(move){
+        chinta.anims.play('move', true);
+      }else{
+        //colocar animação idle aqui
+        chinta.anims.play('move', false);
+      }
+    }
   }
-
-  if(move){
-    chinta.anims.play('move', true);
-  }else{
-    chinta.anims.play('move', false);
-  }
-
 }
